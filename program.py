a1b77c6a4d8ffabd71f5cd54c9a7d6be78edbef9
LijaPietryczuk <87500549+LijaPietryczuk@users.noreply.github.com>
2025-12-14 12:23:51 +0100

    Update program.py

--- program.py
+++ program.py
@@ -1,18 +1,28 @@
-meme_dict = {
-            "CRINGE": "Coś wyjątkowo dziwnego lub zawstydzającego",
-            "LOL": "Częsta reakcja na coś zabawnego",
-            }
-for i in range(5):
-    word = input("Wpisz słowo, którego nie rozumiesz: ").upper()
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'Zalogowaliśmy się jako {bot.user}')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Cześć, jestem bot{bot.user}!')
+
+@bot.command()
+async def heh(ctx, count_heh = 5):
+    await ctx.send("he" * count_heh)
     
-    if word in meme_dict.keys():
-        print('Te słowo jest w słowniku i oznacza ono:')
-        print(meme_dict[word])
-    else:
-        for i in range(5):
-            print('Nie ma tego słowa, możesz je dodać')
-            head = input('Wpisz jakie słowo dodajesz:')
-            body = input('Wpisz jego znaczenie:')
-            meme_dict[head]=body
-            print('Dodano nowe słowo do słownika!')
-            print(head, ':', body)
+@bot.command()
+async def add(ctx, left: int, right: int):
+    """Adds two numbers together."""
+    await ctx.send(left + right)
+
+
+
+bot.run("MTQ0OTU4MTg4ODI1ODcwMzQ1MQ.GUdNj2.Aw-4WeMK29eEive_-rc1e0CMV7alZC8b9cBoQU")
